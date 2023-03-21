1a1af06a4f0782a59ff9809750ca722b345dbf51
Lis <115196622+liscontoli@users.noreply.github.com>
2023-03-21 11:14:04 -0400

    Backend LogIn/SignUp

--- src/Pages/LogIn.js
+++ src/Pages/LogIn.js
@@ -1,32 +1,118 @@
-import React, { Component }  from 'react';
+import React, { useState } from "react";
 const LogIn = () => {
-    return (
-      <div className="Background">
+  // States for user inputs
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  //
+  
+  // Validation states
+  const [showEmailError, setShowEmailError] = useState(false);
+  const [showPasswordError, setShowPasswordError] = useState(false);
+  //
+
+  const [showEmailMessage, setShowEmailMessage] = useState(false);
+  const [showPasswordMessage, setShowPasswordMessage] = useState(false);
+
+  // Constants for error messages
+  const EMAILERRORMESSGAE = "Email must contain @ and .com";
+  const PasswordERRORMESSGAE = "Password must be atleast 8 digits";
+
+  // Function for submiting button and validation
+  const submitButton = () => {
+    if (userEmail === "") {
+      setShowEmailError(true);
+    } else if (userPassword === "") {
+      setShowPasswordError(true);
+    }
+      else if (!validateEmail(userEmail)) {
+        // setShowEmailError(true);
+        setShowEmailMessage(true);
+      } 
+      else if (userPassword.length < 8) {
+        setShowPasswordMessage(true);
+      }
+     else if (validateEmail(userEmail)) {
+        setShowEmailError(false);
+        setShowEmailMessage(false);
+      } 
+      else {
+       
+        setShowPasswordError(false);
+        setShowPasswordMessage(false);
+
+        /* LOGIN DATABASE GOES HERE */
+      }
+  };
+
+  // Validation for email
+  const validateEmail = (email) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  };
+  return (
+    <div className="Background">
       <div className="MainCenterContainer">
         <>
-        <div><span className="titleDE">[DE]</span><span className="titleTHREADER">THREADER</span></div>
+          <div>
+            <span className="titleDE">[DE]</span>
+            <span className="titleTHREADER">THREADER</span>
+          </div>
 
-        <div className="form">
-          <div className="form-body">
-
+          <div className="form">
+            <div className="form-body">
               <div className="email">
-                  <label className="form__label" for="email">Email </label>
-                  <input  type="email" id="email" className="form__input" placeholder="Email"/>
+                <label className="form__label" for="email">
+                  Email{" "}
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className={`form__input ${
+                    showEmailError ? "Show_Error" : ""
+                  }`}
+                  placeholder="Email"
+                  onChange={(e) => setUserEmail(e.target.value)}
+                />
+                {/* Error field for Email message */}
+                {showEmailMessage && (
+                  <div className="Show_Email_Erro">{EMAILERRORMESSGAE}</div>
+                )}
               </div>
               <div className="password">
-                  <label className="form__label" for="password">Password </label>
-                  <input className="form__input" type="password"  id="password" placeholder="Password"/>
-              </div>
+                <label className="form__label" for="password">
+                  Password{" "}
+                </label>
+                <input
+                  className={`form__input ${
+                    showPasswordError ? "Show_Error" : ""
+                  }`}
+                  type="password"
+                  id="password"
+                  placeholder="Password"
+                  onChange={(e) => setUserPassword(e.target.value)}
+                />
 
+                {/* Error field for password message */}
+                {showPasswordMessage && (
+                  <div className="Show_Email_Erro">{PasswordERRORMESSGAE}</div>
+                )}
+              </div>
+            </div>
+            <div class="footer">
+              <button
+                type="submit"
+                className="textButton"
+                onClick={submitButton}
+              >
+                Log In
+              </button>
+            </div>
           </div>
-          <div class="footer">
-              <button type="submit" className="textButton">Log In</button>
-          </div>
-      </div>    
         </>
       </div>
     </div>
-    );
-  }
-  
-  export default LogIn;
+  );
+};
+
+export default LogIn;
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,40 +1,187 @@
-import React, { Component }  from 'react';
-import './SignUp.css';
+import React, { useState } from "react";
+import "./SignUp.css";
 
 const SignUp = () => {
-    return (
-      <div className="Background">
+  // User value states
+  const [userName, setName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  // User value states
+
+  // Error states for checking error
+  const [showError, setShowError] = useState(false);
+  const [showEmailError, setShowEmailError] = useState(false);
+  const [showEmailMessage, setShowEmailMessage] = useState(false);
+  const [showPasswordMessage, setShowPasswordMessage] = useState(false);
+  const [showConfirmdMessage, setShowConfrimMessage] = useState(false);
+  const [showNotSameMessage, setShowNotSameMessage] = useState(false);
+  const EMAILERRORMESSGAE = "Email must contain @ and .com";
+  const PasswordERRORMESSGAE = "Password must be at least 8 digits";
+  const NotSameERRORMESSGAE = "Passwords do not match";
+  // Error states for checking error
+
+  // Function for submiting button and validation
+  const submitButton = () => {
+    
+    if (userName === "") {
+      setShowError(true); // For highlight name field if empty
+    } else if (userEmail === "") {
+      setShowEmailError(true);// For highlight email field if empty
+    } else if (userPassword === "" || userPassword.length < 8) {
+      setShowPasswordMessage(true); // For highlight password field if empty
+    } else if (confirmPassword === "" || confirmPassword.length < 8) {
+      setShowConfrimMessage(true) // For highlight confirm password field if empty
+    }
+     else if (!validateEmail(userEmail)) { // For checking email must
+        // setShowEmailError(true);
+        setShowEmailMessage(true);
+      } 
+        // WHEN ALL CONDITIONS ARE FULFILLED //
+  
+      else {
+            if (validateEmail(userEmail)) { // Hiding the email message if validate email
+          setShowEmailMessage(false);
+        } 
+       
+       if (userPassword !== confirmPassword) { // For matching both password and confirm passwords are same 
+        // console.log('called');
+        setShowNotSameMessage(true);
+      }
+      if (userPassword == confirmPassword) { // For matching both password and confirm passwords are same 
+        // console.log('called');
+        setShowNotSameMessage(false);
+      }
+      // ALL VALIDATION IS DONE HERE //
+
+      else{ 
+        
+      // DATABASE // sign up database logic here
+
+      }
+       
+      }
+    // }
+  };
+
+  // Validation code for email
+  const validateEmail = (email) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  };
+  return (
+    <div className="Background">
       <div className="MainCenterContainer">
         <>
-        <div><span className="titleDE">[DE]</span><span className="titleTHREADER">THREADER</span></div>
+          <div>
+            <span className="titleDE">[DE]</span>
+            <span className="titleTHREADER">THREADER</span>
+          </div>
 
-        <div className="form">
-          <div className="form-body">
+          <div className="form">
+            <div className="form-body">
               <div className="username">
-                  <label className="form__label" for="firstName">First Name </label>
-                  <input className="form__input" type="text" id="firstName" placeholder="First Name"/>
+                <label className="form__label" for="firstName">
+                  First Name{" "}
+                </label>
+                <input
+                  className={`form__input ${showError ? "Show_Error" : ""}`}
+                  type="text"
+                  id="firstName"
+                  placeholder="First Name"
+                  onChange={(e) => {
+                    setName(e.target.value);
+                    setShowError(false);
+                  }}
+                />
               </div>
+
               <div className="email">
-                  <label className="form__label" for="email">Email </label>
-                  <input  type="email" id="email" className="form__input" placeholder="Email"/>
+                <label className="form__label" for="email">
+                  Email{" "}
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className={`form__input ${
+                    showEmailError ? "Show_Error" : ""
+                  }`}
+                  placeholder="Email"
+                  // Function for view  changing of values
+                  onChange={(e) => { 
+                    setUserEmail(e.target.value);
+                    setShowEmailError(false);
+                  }}
+                />
+                {/* Error messaege div we will show and hide based on check */}
+                {showEmailMessage && (
+                  <div className="Show_Email_Erro">{EMAILERRORMESSGAE}</div>
+                )}
               </div>
               <div className="password">
-                  <label className="form__label" for="password">Password </label>
-                  <input className="form__input" type="password"  id="password" placeholder="Password"/>
+                <label className="form__label" for="password">
+                  Password{" "}
+                </label>
+                <input
+                  className={`form__input ${
+                    showPasswordMessage ? "Show_Error" : ""
+                  }`}
+                  type="password"
+                  id="password"
+                  placeholder="Password"
+                   // Function for view  changing of values
+                  onChange={(e) => {
+                    setUserPassword(e.target.value);
+                    setShowPasswordMessage(false);
+                  }}
+                />
+                 {/* Error message div we will show and hide based on check */}
+                {showPasswordMessage && (
+                  <div className="Show_Email_Erro">{PasswordERRORMESSGAE}</div>
+                )}
               </div>
               <div className="confirm-password">
-                  <label className="form__label" for="confirmPassword">Confirm Password </label>
-                  <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"/>
+                <label className="form__label" for="confirmPassword">
+                  Confirm Password{" "}
+                </label>
+                <input
+                  className={`form__input ${
+                    showConfirmdMessage ? "Show_Error" : ""
+                  }`}
+                  type="password"
+                  id="confirmPassword"
+                  placeholder="Confirm Password"
+                   // Function for view changing of values
+                  onChange={(e) => {
+                    setConfirmPassword(e.target.value);
+                    setShowConfrimMessage(false)
+                  }}
+                />
+                {showConfirmdMessage && (
+                  <div className="Show_Email_Erro">{PasswordERRORMESSGAE}</div>
+                )}
+                {/* Message for passwords NOT matching (not same) */}
+                {/* <div className="Show_Email_Erro">{NotSameERRORMESSGAE}</div> */}
+                {showNotSameMessage && (
+                  <div className="Show_Email_Erro">{NotSameERRORMESSGAE}</div>
+                )}
               </div>
+            </div>
+            <div class="footer">
+              <button
+                type="submit"
+                className="textButton"
+                onClick={submitButton}
+              >
+                Sign Up
+              </button>
+            </div>
           </div>
-          <div class="footer">
-              <button type="submit" className="textButton">Sign Up</button>
-          </div>
-      </div>    
         </>
       </div>
     </div>
-    );
-  }
-  
-  export default SignUp;
+  );
+};
+
+export default SignUp;
